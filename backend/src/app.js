156e64cc270c91ee54f865e97cc19cf3b458489c
shreekar-reddy-sg <shreekarreddysg520@gmail.com>
2026-03-22 20:22:18 +0530

    Added Login Page UI

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,8 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 import shopRouter from "./routes/shop.route.js";
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import axios from "axios";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = async (e) => {
+    e.preventDefault(); // stop page reload
+
+    try {
+      const res = await axios.post("http://localhost:4000/api/auth/login", {
+        email,
+        password
+      });
+      localStorage.setItem("token", res.data.token);
+
+      alert("Login successful");
+
+    } catch (error) {
+      alert(error.response?.data?.message || "Login failed");
+    }
+  };
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      
+      <form onSubmit={handleLogin} style={{ border: "1px solid black", padding: "20px" }}>
+        
+        <h2>Login</h2>
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br /><br />
+
+        <button type="submit">Login</button>
+
+      </form>
+
+    </div>
+  );
+};
+
+export default Login;
